bdc49b54c03d8a71a7043d5e79f6bd85667df0e7
Mark Bykhovets <mb@hrsmnt.ru>
2020-05-25 14:37:46 +0000

    deploy settings

---
+++ technoinst/gunicorn_config.py
@@ -0,0 +1,8 @@
+command = '/home/mark/by-mark.ru/env/bin/gunicorn'
+pythonpath = '/home/mark/by-mark.ru/technoinst'
+bind = '127.0.0.1.:8005'
+workers = 5
+user = 'mark'
+limit_request_fields = 32000
+limit_request_lield = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=juntracker.settings'
